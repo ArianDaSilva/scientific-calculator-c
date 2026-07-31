de51ed0fc36cde35c0f817e72be56728a5614f2f
manugolem <manuelcarreras2013@gmail.com>
2026-07-30 21:56:53 -0300

    main loop definition

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdbool.h>
 #include <stdio.h>
+void menu() {
+    printf("\nCLI Calculator");
+    printf("\n[ 1 ] Basic Arithmetics.");
+    printf("\n[ 2 ] Advanced Options.");
+    printf("\n[ 3 ] System Options.");
+    printf("\n[ 4 ] Exit.");
+    printf("\nPlease enter an option from the main menu:");
+}
+void basic() {
+}
+void advanced_option() {
+}
+void system_options() {
+}
 int main() {
     bool exit_calc = false;
     int input;
@@ -10,7 +24,19 @@ int main() {
         printf("Press [ENTER] to access the main menu...");
         input = getchar();
         if (input == '\n') {
-            exit_calc = true;
+            menu();
+            input = getchar();
+            if (input == '1') {
+                basic();
+            } else if (input == '2') {
+                advanced_option();
+            } else if (input == '3') {
+                system_options();
+            } else if (input == '4') {
+                exit_calc = true;
+            } else {
+                menu();
+            }
         }
     }
     return 0;
