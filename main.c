51e41f0144ac8d6c37c25b6cfc85c2d2d150ca60
ManuGolem <manuelcarreras2013@gmail.com>
2026-08-01 12:38:46 -0300

    make advanced arithmetics

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -5,6 +6,11 @@
 // void menu(void);
 void menu()
 {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
     printf("\nCLI Calculator");
     printf("\n[ 1 ] Basic Arithmetics.");
     printf("\n[ 2 ] Advanced Options.");
@@ -21,11 +27,11 @@ void basicArithmetic(void)
     int x, y;
     do
     {
-#ifdef _WIN32
-        system("cls");
-#else
-        system("clear");
-#endif
+        #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
         printf("\n%-22s %-16s %s\n", "Name", "Example", "Description");
         printf("%-22s %-16s %s\n", "[ 1 ] Addition", "x + y", "Adds together two values");
         printf("%-22s %-16s %s\n", "[ 2 ] Subtraction", "x - y", "Subtracts one value from another");
@@ -94,8 +100,72 @@ void basicArithmetic(void)
     exit(0);
 }
 
+//Advanced arithmetic operations menu
 void advanced_option()
 {
+    int option;
+    int x, y;
+    bool exit_loop=false;
+    while(!exit_loop)
+    {
+        #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
+        printf("\n%-22s %-16s %s\n", "Name", "Example", "Description");
+        printf("%-22s %-16s %s\n", "[ 1 ] Exponentiation", "x^y", "Raises a base number to the power of an exponent");
+        printf("%-22s %-18s %s\n", "[ 2 ] Square Root", "√x", "Finds the number equals to the input when multiplied by itself");
+        printf("%-22s %-16s %s\n", "[ 3 ] Mudulus", "x % y", "Returns the remainder left over after integer division");
+        printf("%-22s %-16s %s\n", "[ 4 ] Perecentage", "x%", "Calculate a specified portion out of a total of 100");
+        printf("[ 5 ] Return to the main menu..\n");
+        printf("[ 6 ] Exit the program.\n");
+        printf("Please select an option: ");
+        scanf("%d", &option);
+        if(option==5) return;
+        if(option==6)
+        {
+            printf("Exiting the program...\n");
+            exit(0);
+        }
+        if(option == 1 || option == 3)
+        {
+            printf("\nEnter first number: ");
+            scanf("%d", &x);
+            printf("Enter second number: ");
+            scanf("%d", &y);
+        }
+        switch (option)
+        {
+            case 1:
+                printf("\n--- Result ---\n");
+                printf("%d^%d = %d\n", x, y, pow(x,y));//2 decimales 
+                break;
+            case 2:
+                printf("\nEnter a number: ");
+                scanf("%d", &x);
+                printf("\n--- Result ---\n");
+                printf("√%d = %.2f\n",x,sqrt((double)x));
+                break;
+            case 3:
+                printf("\n--- Result ---\n");
+                printf("%d %% %d = %d\n",x,y,x%y);
+                break;
+            case 4:
+                printf("\nEnter a number: ");
+                scanf("%d", &x);
+                printf("\n--- Result ---\n");
+                printf("%d%% = %.2f\n",x,(double)x/100);
+                break;
+            default:
+                printf("Error: Invalid option.\n");
+                break;
+        }
+        printf("\nDo you want to perform another operation? (y/n): ");
+        char choice;
+        scanf(" %c", &choice);
+        exit_loop = (choice == 'n' || choice == 'N');
+    }
 }
 void system_options()
 {
@@ -104,12 +174,12 @@ int main()
 {
     bool exit_calc = false;
     int input;
+    printf("\n===========================\n");
+    printf("WELCOME TO CLI CALCULATOR!");
+    printf("\n===========================\n");
+    printf("Press [ENTER] to access the main menu...");
     while (!exit_calc)
     {
-        printf("\n===========================\n");
-        printf("WELCOME TO CLI CALCULATOR!");
-        printf("\n===========================\n");
-        printf("Press [ENTER] to access the main menu...");
         input = getchar();
         if (input == '\n')
         {
